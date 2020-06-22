4859e59c3ebc029ce1c6153df28387794bb024eb
Tim Costa <costa.tim@live.be>
2020-06-22 21:15:15 +0200

    Updates

---
+++ precache-manifest.01412b43a541a3d6a66db7687a1fd6d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174c3a1773ad422ce43ef3544f2d9fdf",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "c843317a4f12000ad3cf",
+    "url": "/tictactoe/static/css/main.0de2b074.chunk.css"
+  },
+  {
+    "revision": "c590bf2898e0fcd3894e",
+    "url": "/tictactoe/static/js/2.527c47db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.527c47db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c843317a4f12000ad3cf",
+    "url": "/tictactoe/static/js/main.c44e38de.chunk.js"
+  },
+  {
+    "revision": "7154fe3e576d72fb6176",
+    "url": "/tictactoe/static/js/runtime-main.a5be6b49.js"
+  }
+]);
